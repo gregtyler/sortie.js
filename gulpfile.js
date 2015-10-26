d1783d364b8278128ad506fe31b4de13645f2ad3
Greg Tyler <greg@gregtyler.co.uk>
2015-10-26 18:31:37 +0000

    Change of structure and syntax + sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,25 @@
+// Load modules
 const gulp = require('gulp');
 const jscs = require('gulp-jscs');
 const rename = require('gulp-rename');
 const replace = require('gulp-replace');
+const sourcemaps  = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 
+// Load data from package.json
 var pkg = require('./package.json');
 
+// The default build task
 gulp.task('default', function() {
   return gulp.src('src/sortie.js')
+    .pipe(sourcemaps.init())
     .pipe(jscs()) //JSCS
     .pipe(jscs.reporter())
     .pipe(replace('<<VERSION>>', pkg.version))
     .pipe(gulp.dest('build')) // Output file
-    .pipe(uglify({preserveComments: 'license'})) // Uglify
+    .pipe(uglify({outSourceMap: true, preserveComments: 'license'})) // Uglify
     .pipe(rename({extname: '.min.js'})) // Rename
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('build')) // Output minified
   ;
 });
--- src/sortie.js
+++ src/sortie.js
@@ -2,9 +2,12 @@
  * Sortie.js v<<VERSION>>
  * An inoffensive table sorter for developers
  */
-function Sortie(el, options) {
+window.Sortie = (function SortieConstructor() {
     'use strict';
 
+   /**
+    * Variable definitions
+    */
     var defaults = {
         initialsort: -1,
         markers: {
@@ -14,28 +17,7 @@ function Sortie(el, options) {
         }
     };
 
-    // Get the variables we'll be using in this sortie object
-    var $table = $(el);
-    var $body = $table.find('tbody').eq(0);
-    var $headers = $table.find('thead th');
-    var current;
-    var options;
-
-   /**
-    * Get the options object
-    */
-    function getOptions() {
-        var dataAttr = $table.data();
-        var dataOptions = {};
-        for (var key in dataAttr) {
-            if (key.substr(0, 6) === 'sortie' && key.length > 6) {
-                var normkey = key.toLowerCase().replace(/-/g, '');
-                dataOptions[normkey.substr(6)] = dataAttr[key];
-            }
-        }
-
-        return $.extend(true, {}, defaults, options, dataOptions);
-    }
+    var compareFunctions = {};
 
    /**
     * Reverse a string
@@ -72,176 +54,225 @@ function Sortie(el, options) {
         return node.textContent || $(node).text();
     }
 
-   /**
-    * Initialise sortie
-    */
-    function init() {
-        // Set the options object
-        options = getOptions();
+    function SortieInstance(el, options) {
+        // Get the variables we'll be using in this sortie object
+        var $table = $(el);
+        var $body = $table.find('tbody').eq(0);
+        var $headers = $table.find('thead th');
+        var current;
 
-        // Determine the sort order
-        var sorts = options.sort || $table.attr('data-sortie');
-        if (!typeof sorts !== 'object') {
-            sorts = sorts.split(',');
-        }
-
-        if (sorts.length > $headers.length) {
-            console.error('Sortie config error: Too many sort columns specified');
-        }
-
-        // Add aria role-live settings and
-        $table.attr({
-            'aria-atomic': true,
-            'aria-live': 'polite',
-            'aria-relevant': 'all'
-        });
-
-        // Generate an ID for the table
-        if (!$table.attr('id')) {
-            $table.attr('id', 'sortieTable' + (new Date() * 1));
-        }
-
-        // Add sortie specification to each column
-        for (var col in sorts) {
-            // Skip columns with no sort criteria
-            if (!sorts[col]) continue;
-
-            // Add a sorting icon to click
-            var $button = $('<button />').css({backgroundColor: 'transparent', border: 'none'}).html(options.markers.unsorted);
-            $button.attr('aria-controls', $table.attr('id'));
-            $headers.eq(col)
-                .css({whiteSpace:'nowrap', cursor:'pointer'})
-                .click(sortFactory(col))
-                .data({
-                    sortie: true,
-                    sortieCompareFunction: compareFactory(col, sorts[col]),
-                    sortieButton: $button
-                })
-                .append(' ', $button);
-        }
-
-        // Perform an initial sort
-        if (options.initialsort !== false) {
-            var opts = {};
-            if (options.initialsort.toString().substr(-1, 1) === 'D') {
-                opts.dir = 'DESC';
-            } else if (options.initialsort.toString().substr(-1, 1) === 'A') {
-                opts.dir = 'ASC';
-            } else if (options.initialsort.toString() === '-1') {
-                for (var i = 0, l = sorts.length; i < l; i++) {
-                    if (sorts[i]) {
-                        options.initialsort = i;
-                        break;
-                    }
+       /**
+        * Get the options object
+        */
+        function getOptions() {
+            var dataAttr = $table.data();
+            var dataOptions = {};
+            for (var key in dataAttr) {
+                if (key.substr(0, 6) === 'sortie' && key.length > 6) {
+                    var normkey = key.toLowerCase().replace(/-/g, '');
+                    dataOptions[normkey.substr(6)] = dataAttr[key];
                 }
             }
 
-            sort(options.initialsort, opts);
-        }
-    }
-
-    // Factory for the sort function
-    function sortFactory(col) {
-        return function() {
-            sort(col);
-        }
-    }
-
-   /**
-    * Sort the table by the given column
-    */
-    function sort(col, opts) {
-        // Convert col to an integer
-        col = parseInt(col, 10);
-
-        var opts = $.extend({}, opts);
-        var $th = $headers.eq(col);
-        var $btn = $th.data('sortieButton');
-        var rows = Array.prototype.slice.call($body.find('tr').get(), 0);
-        var dir = typeof opts.dir !== 'undefined' ? opts.dir : 'ASC';
-
-        // Get the sort order
-        if (current === col && $th.data('sortieDirection') === 'ASC' && typeof opts.dir == 'undefined') {
-            dir = 'DESC';
+            return $.extend(true, {}, defaults, options, dataOptions);
         }
 
-        // Save the new state
-        current = col;
-        $th.data('sortieDirection', dir);
-        $th.attr('aria-sort', dir.toLowerCase() + 'ending')
+       /**
+        * Initialise sortie
+        */
+        function init() {
+            // Set the options object
+            options = getOptions();
 
-        // Perform the sort
-        rows.sort($th.data('sortieCompareFunction'));
-
-        // Update the button's HTML
-        if (dir === 'DESC') {
-            $btn.html(options.markers.desc);
-            rows = rows.reverse();
-        } else {
-            $btn.html(options.markers.asc);
-        }
-
-        // Update the other buttons
-        $headers.not($th).each(function(index, el) {
-            var $el = $(el);
-            $el.removeAttr('aria-sort');
-            if ($el.data('sortie')) {
-                $el.data('sortieButton').html(options.markers.unsorted);
+            // Determine the sort order
+            var sorts = options.sort || $table.attr('data-sortie');
+            if (!typeof sorts !== 'object') {
+                sorts = sorts.split('|');
             }
-        });
 
-        // Update the content of the now-sorted table
-        var frag = document.createDocumentFragment();
-        var tbody = $body.get(0);
+            if (sorts.length > $headers.length) {
+                console.error('Sortie config error: Too many sort columns specified');
+            }
 
-        for (var i in rows) {
-            frag.appendChild(rows[i]);
+            // Add aria role-live settings and
+            $table.attr({
+                'aria-atomic': true,
+                'aria-live': 'polite',
+                'aria-relevant': 'all'
+            });
+
+            // Generate an ID for the table
+            if (!$table.attr('id')) {
+                $table.attr('id', 'sortieTable' + (new Date() * 1));
+            }
+
+            // Add sortie specification to each column
+            for (var col in sorts) {
+                // Skip columns with no sort criteria
+                if (!sorts[col]) continue;
+
+                // Add a sorting icon to click
+                var $button = $('<button />').css({backgroundColor: 'transparent', border: 'none'}).html(options.markers.unsorted);
+                $button.attr('aria-controls', $table.attr('id'));
+                $headers.eq(col)
+                    .css({whiteSpace:'nowrap', cursor:'pointer'})
+                    .click(sortFactory(col))
+                    .data({
+                        sortie: true,
+                        sortieCompareFunction: compareFactory(col, sorts[col]),
+                        sortieButton: $button
+                    })
+                    .append(' ', $button);
+            }
+
+            // Perform an initial sort
+            if (options.initialsort !== false) {
+                var opts = {};
+                if (options.initialsort.toString().substr(-1, 1) === 'D') {
+                    opts.dir = 'DESC';
+                } else if (options.initialsort.toString().substr(-1, 1) === 'A') {
+                    opts.dir = 'ASC';
+                } else if (options.initialsort.toString() === '-1') {
+                    for (var i = 0, l = sorts.length; i < l; i++) {
+                        if (sorts[i]) {
+                            options.initialsort = i;
+                            break;
+                        }
+                    }
+                }
+
+                sort(options.initialsort, opts);
+            }
         }
 
-        // Put the now sorted rows back onto the page
-        tbody.innerHTML = '';
-        tbody.appendChild(frag);
+        // Factory for the sort function
+        function sortFactory(col) {
+            return function() {
+                sort(col);
+            }
+        }
 
-        $table.trigger('sortie:sorted');
+       /**
+        * Sort the table by the given column
+        */
+        function sort(col, opts) {
+            // Convert col to an integer
+            col = parseInt(col, 10);
+
+            var opts = $.extend({}, opts);
+            var $th = $headers.eq(col);
+            var $btn = $th.data('sortieButton');
+            var rows = Array.prototype.slice.call($body.find('tr').get(), 0);
+            var dir = typeof opts.dir !== 'undefined' ? opts.dir : 'ASC';
+
+            // Get the sort order
+            if (current === col && $th.data('sortieDirection') === 'ASC' && typeof opts.dir == 'undefined') {
+                dir = 'DESC';
+            }
+
+            // Save the new state
+            current = col;
+            $th.data('sortieDirection', dir);
+            $th.attr('aria-sort', dir.toLowerCase() + 'ending')
+
+            // Perform the sort
+            rows.sort($th.data('sortieCompareFunction'));
+
+            // Update the button's HTML
+            if (dir === 'DESC') {
+                $btn.html(options.markers.desc);
+                rows = rows.reverse();
+            } else {
+                $btn.html(options.markers.asc);
+            }
+
+            // Update the other buttons
+            $headers.not($th).each(function(index, el) {
+                var $el = $(el);
+                $el.removeAttr('aria-sort');
+                if ($el.data('sortie')) {
+                    $el.data('sortieButton').html(options.markers.unsorted);
+                }
+            });
+
+            // Update the content of the now-sorted table
+            var frag = document.createDocumentFragment();
+            var tbody = $body.get(0);
+
+            for (var i in rows) {
+                frag.appendChild(rows[i]);
+            }
+
+            // Put the now sorted rows back onto the page
+            tbody.innerHTML = '';
+            tbody.appendChild(frag);
+
+            $table.trigger('sortie:sorted');
+        }
+
+       /**
+        * Return the suitable comparison function for the column
+        */
+        function compareFactory(col, sortSpec) {
+            var bits = sortSpec.split(':');
+            var operation = bits[0];
+            var arguments = bits[1] ? bits[1].split(',') : [];
+            var callback = compareFunctions[operation];
+
+            if (typeof callback !== 'function') {
+                console.error('Sortie error: comparison function not found for rule "' + operation + '"');
+                var callback = function() {return 0};
+            }
+
+            return function compare(a, b) {
+                var cellA = a.children[col];
+                var cellB = b.children[col];
+
+                var order = callback.call(window, cellA, cellB, arguments);
+
+                // If asked to, reverse the search order
+                return sortSpec.substr(1, 1) === 'r' ? -order : order;
+            }
+        }
+
+        // Run the initialisation
+        init();
+
+        // Return values
+        var obj = { sort: sort };
+        $table.data('mySortieInstance', obj);
+        return obj;
     }
 
-   /**
-    * Return the suitable comparison function for the column
-    */
-    function compareFactory(col, sortSpec) {
-        var callback = function() {return 0};
+    // Create a new sortie instance
+    function create(el, options) {
+        return new SortieInstance(el, options);
+    }
 
-        if (sortSpec.substr(0, 1) === 'a') {
-            callback = compareAlpha;
-        } else if (sortSpec.substr(0, 1) === 'd') {
-            callback = compareDate;
-        } else if (sortSpec.substr(0, 1) === 'i') {
-            callback = compareInt;
-        }
+    // Register a new comparison function
+    function registerComparison(codes, callback) {
+        if (typeof codes === 'string') codes = [codes];
 
-        return function compare(a, b) {
-            var cellA = a.children[col];
-            var cellB = b.children[col];
-
-            var order = callback.call(window, cellA, cellB, sortSpec);
-
-            // If asked to, reverse the search order
-            return sortSpec.substr(1, 1) === 'r' ? -order : order;
+        // Register against each code provided
+        for (var i = 0, l = codes.length; i < l; i++) {
+            compareFunctions[codes[i]] = callback;
         }
     }
+
+    /*** Comparison functions ***/
 
     // Compare alphanumerically
-    function compareAlpha(cellA, cellB) {
+    registerComparison(['a', 'alpha'], function compareAlpha(cellA, cellB) {
         var ah = textContent(cellA).trim();
         var bh = textContent(cellB).trim();
         if (cellA.dir === 'rtl') ah = strReverse(ah);
         if (cellB.dir === 'rtl') bh = strReverse(bh);
         return ah.localeCompare(bh);
-    }
+    });
 
-    // Compare dates
-    function compareDate(cellA, cellB, sortSpec) {
-        var dateFormat = sortSpec.substr(2);
+    // Compare by date attributes
+    registerComparison(['d', 'date'], function compareData(cellA, cellB, args) {
+        var dateFormat = args[0];
         var dateA = parseDate(textContent(cellA), dateFormat);
         var dateB = parseDate(textContent(cellB), dateFormat);
 
@@ -252,19 +283,10 @@ function Sortie(el, options) {
         } else {
             return 0;
         }
-    }
-
-    // Compare by data attributes
-    function compareData(cellA, cellB) {
-        var aVal = parseFloat(cellA.getAttribute('data-val'));
-        var bVal = parseFloat(cellB.getAttribute('data-val'));
-        var aTotal = parseFloat(cellA.getAttribute('data-total')) || 0;
-        var bTotal = parseFloat(cellB.getAttribute('data-total')) || 0;
-        return bVal - aVal || bTotal - aTotal;
-    }
+    });
 
     // Compare integers
-    function compareInt(cellA, cellB) {
+    registerComparison(['i', 'int'], function compareInt(cellA, cellB) {
         var aInt = parseInt(textContent(cellA), 10);
         var bInt = parseInt(textContent(cellB), 10);
         if (isNaN(bInt)) {
@@ -274,15 +296,26 @@ function Sortie(el, options) {
         } else {
             return aInt - bInt;
         }
-    }
+    });
 
-    // Run the initialisation
-    init();
-}
+    return {
+        registerComparison: registerComparison,
+        create: create
+    };
+})();
 
 // Attach sortie to jQuery
 $.fn.sortie = function(options) {
-    $(this).each(function() {
-        Sortie(this, options);
-    }
-)};
+    var args = Array.prototype.slice.apply(arguments);
+
+    return $(this).each(function() {
+        if (typeof options === 'string') {
+            var command = options;
+            $(this).data('mySortieInstance')[command](args.slice(1));
+        } else {
+            if (!$(this).data('mySortieInstance')) {
+                Sortie.create(this, options);
+            }
+        }
+    });
+}
